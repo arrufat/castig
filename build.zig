267bde4619b0fe8257e9f20f7a81f589374c4237
Adrià Arrufat <adria.arrufat@gmail.com>
2026-09-20 00:25:23 +0200

    build: strip non debug builds

--- build.zig
+++ build.zig
@@ -32,6 +32,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
         .link_libc = true,
+        .strip = optimize != .debug,
         .imports = &.{
             .{ .name = "av", .module = av },
         },
